d27d54765bb4d679d475fa702e7faa3c0245ab8d
0emre.ozkaya0@gmail.com <0emre.ozkaya0@gmail.com>
2021-06-20 11:43:07 +0300

    HRMS

--- src/main/java/kodlamaio/hrms/core/adapters/MernisServiceAdapter.java
+++ src/main/java/kodlamaio/hrms/core/adapters/MernisServiceAdapter.java
@@ -1,16 +1,18 @@
 package kodlamaio.hrms.core.adapters;
 
-import java.rmi.RemoteException;
-
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.core.abstracts.MernisValidationService;
 import kodlamaio.hrms.entities.concretes.JobSeeker;
+
 @Service
-public class MernisServiceAdapter  implements MernisValidationService{
+public class MernisServiceAdapter implements MernisValidationService {
+
+	@Override
+	public boolean validate(JobSeeker jobSeeker) {
+		// FAKE DOĞRULAMA SİSTEMİDİR.
 
 		return true;
 	}
-	
-	
+
 }
